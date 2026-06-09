0b0294f051fd47a1aac47922f681cff8dbc1c52e
Pedro Goncalves <pedrogoncalvesr89@gmail.com>
2026-06-09 15:21:34 -0300

    Volta codigo para commit do readme

--- src/main/java/br/com/alertaorbital/bo/TipoDesastreBO.java
+++ src/main/java/br/com/alertaorbital/bo/TipoDesastreBO.java
@@ -4,18 +4,15 @@ import br.com.alertaorbital.dao.TipoDesastreDAO;
 import br.com.alertaorbital.entities.TipoDesastre;
 import br.com.alertaorbital.excecoes.ExcecoesConexao;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class TipoDesastreBO {
 
-    private static final List<String> NIVEIS = Arrays.asList("BAIXO", "MEDIO", "ALTO", "CRITICO");
-
     public TipoDesastre cadastrar(TipoDesastre td) throws ExcecoesConexao {
         if (td.getNome() == null || td.getNome().isBlank())
             throw new ExcecoesConexao("nome do tipo de desastre é obrigatorio");
-        if (td.getNivelRisco() == null || !NIVEIS.contains(td.getNivelRisco().toUpperCase()))
-            throw new ExcecoesConexao("nivel_risco invalido. Use: BAIXO, MEDIO, ALTO ou CRITICO");
+        if (td.getNivelRisco() == null || td.getNivelRisco().isBlank())
+            throw new ExcecoesConexao("nivel_risco é obrigatorio");
 
         td.setNivelRisco(td.getNivelRisco().toUpperCase());
         TipoDesastreDAO dao = new TipoDesastreDAO();
@@ -24,20 +21,18 @@ public class TipoDesastreBO {
     }
 
     public List<TipoDesastre> listar() throws ExcecoesConexao {
-        TipoDesastreDAO dao = new TipoDesastreDAO();
-        return dao.listar();
+        return new TipoDesastreDAO().listar();
     }
 
     public TipoDesastre buscarPorId(int id) throws ExcecoesConexao {
-        TipoDesastreDAO dao = new TipoDesastreDAO();
-        return dao.buscarPorId(id);
+        return new TipoDesastreDAO().buscarPorId(id);
     }
 
     public TipoDesastre atualizar(TipoDesastre td) throws ExcecoesConexao {
         if (td.getNome() == null || td.getNome().isBlank())
             throw new ExcecoesConexao("nome do tipo de desastre é obrigatorio");
-        if (td.getNivelRisco() == null || !NIVEIS.contains(td.getNivelRisco().toUpperCase()))
-            throw new ExcecoesConexao("nivel_risco invalido. Use: BAIXO, MEDIO, ALTO ou CRITICO");
+        if (td.getNivelRisco() == null || td.getNivelRisco().isBlank())
+            throw new ExcecoesConexao("nivel_risco é obrigatorio");
 
         td.setNivelRisco(td.getNivelRisco().toUpperCase());
         TipoDesastreDAO dao = new TipoDesastreDAO();
@@ -46,7 +41,6 @@ public class TipoDesastreBO {
     }
 
     public void deletar(int id) throws ExcecoesConexao {
-        TipoDesastreDAO dao = new TipoDesastreDAO();
-        dao.deletar(id);
+        new TipoDesastreDAO().deletar(id);
     }
 }
